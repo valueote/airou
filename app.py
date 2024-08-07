968577f7f2f891742634a83b9ee82ec3333e92cd
vivy <2686220852@qq.com>
2024-08-07 16:50:53 +0800

    change app.py

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ def get_vectordb():
     """
 
     EMBEDDING_DEVICE = "cpu"
-    embeddings = HuggingFaceEmbeddings(model_name="/home/vivy/ai/m3e-base",
+    embeddings = HuggingFaceEmbeddings(model_name="./m3e-base",
                                        model_kwargs={'device': EMBEDDING_DEVICE})
 
     # 从本地文件加载向量数据库
